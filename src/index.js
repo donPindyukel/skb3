3d939b000b9560de9a3ffd256b3c435936d7c417
Andrey Pindyuk <a913000@gmail.com>
2016-11-13 14:04:09 +0700

    express wrap and routers added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
-//import express from  'express';
-//import cors from 'cors';
-
+import express from  'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import saveDataDb from './saveDataInDb';
+import Pet from './models/Pet';
+import User from './models/User';
 
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://publicdb.mgbeta.ru/pindyuk_skb3');
 
-const data = {
-  user: {
-    name: 'pindyuk',
-  },
-  pets: [
-    {
-      name: 'Zildjian',
-      type: 'cat',
-    },
-    {
-      name: 'Doge',
-      type: 'dog'
-    },
-  ]
-};
+const app = express();
+app.use(cors());
 
-saveDataDb(data);
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  return res.json(users);
+});
+
+app.get('/pets', async (req, res) => {
+  const pets = await Pet.find();
+  return res.json(pets);
+});
+
+app.post('/data', async (req, res) => {
+  const pets = req.body;
+  /*const data = {
+    user: {
+      name: 'pindyuk',
+    },
+    pets: [
+      {
+        name: 'Zildjian',
+        type: 'cat',
+      },
+      {
+        name: 'Doge',
+        type: 'dog'
+      },
+    ]
+  };*/
+  saveDataDb(data);
+});
 
 /*const kitty = new Pet({
   name: 'Ziljian',
@@ -42,11 +58,6 @@ kitty.save()
 
 //const app = express();
 //app.use(cors());
-
-
-
-
-
-/*app.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
-});*/
+});
